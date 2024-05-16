35d9920bb0da5583fc8653b65bd86a7b5fd0ec05
gt-min <gt_min@naver.com>
2024-05-16 14:40:44 +0900

    20240516

---
+++ 03_control/src/pkg02_switch/SwitchEx.java
@@ -0,0 +1,16 @@
+package pkg02_switch;
+
+public class SwitchEx {
+
+  public static void main(String[] args) {
+    
+    /*
+     * switch
+     * 1. 표현식의 결과값에 따라서 분기 처리하는 제어문이다.
+     * 2. 표현식의 결과값은 byte, short, int, char, String 타입 중 하나를 가져야 한다.
+     *    boolean, long, float, double 타입은 표현식의 결과값이 될 수 없다.
+     */
+
+  }
+
+}
