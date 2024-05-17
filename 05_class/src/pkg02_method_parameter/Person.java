decfa92d878a9a42caae2f03133d4c170df9d052
gt-min <gt_min@naver.com>
2024-05-17 15:20:20 +0900

    20240517

---
+++ 05_class/src/pkg02_method_parameter/Person.java
@@ -0,0 +1,33 @@
+package pkg02_method_parameter;
+
+public class Person {
+
+  /*
+   * 메소드의 구성
+   * 1. 반환타입 : 메소드를 호출하면 메소드가 실행되고 어떤 값을 반환하는데 그 값의 타입
+   * 2. 메소드명
+   * 3. 매개변수 : parameter 라고 한다. 메소드를 호출할 때 전달되는 값(인자, argument)을 저장하는 변수
+   */
+  
+  /*
+   * 메소드의 형식
+   *   반환타입 메소드명(매개변수) {
+   *     메소드 본문
+   *   }
+   */
+
+  void method1(int number) {
+    System.out.println("method1:" + number);
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+}
---
+++ 05_class/src/pkg02_method_parameter/PersonEx.java
@@ -0,0 +1,16 @@
+package pkg02_method_parameter;
+
+public class PersonEx {
+
+  public static void main(String[] args) {
+    
+    // 클래스 Person 타입의 인스턴스 선언과 생성
+    Person person = new Person();
+    
+    // 인스턴스 person 의 메소드 호출하기
+    person.method1(10);
+    
+    
+  }
+
+}
