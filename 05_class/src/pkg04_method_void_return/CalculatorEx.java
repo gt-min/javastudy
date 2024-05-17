56a35a714b766e4ff8f3667f59deaea2624f36b8
gt-min <gt_min@naver.com>
2024-05-17 18:32:52 +0900

    20240517

---
+++ 05_class/src/pkg04_method_void_return/CalculatorEx.java
@@ -0,0 +1,13 @@
+package pkg04_method_void_return;
+
+public class CalculatorEx {
+
+  public static void main(String[] args) {
+    
+    Calculator calculator = new Calculator();
+    
+    calculator.root(-9);
+
+  }
+
+}
