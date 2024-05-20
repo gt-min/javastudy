9c77ecb80fa55e981eda81a51840ea70457d38ba
gt-min <gt_min@naver.com>
2024-05-20 15:18:23 +0900

    20240520

---
+++ 05_class/src/ex6/BankAccount.java
@@ -0,0 +1,26 @@
+package ex6;
+
+public class BankAccount {
+
+  // 계좌 잔액을 저장할 balance 필드 선언
+  long balance;
+  
+  // 계좌 번호를 저장할 accNo 필드 선언
+  String accNo;
+  
+  // 입금 (마이너스 입금 방지)
+  void deposit(long money) {
+    
+  }
+  
+  // 출금 (실제 출금액을 반환 / 계좌 잔액보다 큰 출금 방지)
+  long withdrawal(long money) {
+    
+  }
+  
+  // 계좌 정보 조회 (계좌 번호 / 잔액 출력)
+  void inquiry() {
+    
+  }
+  
+}
---
+++ 05_class/src/ex6/BankAccountEx.java
@@ -0,0 +1,11 @@
+package ex6;
+
+public class BankAccountEx {
+
+  public static void main(String[] args) {
+    
+    // TODO Auto-generated method stub
+
+  }
+
+}
