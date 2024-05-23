ec572697c0fbca269cdc07c85e536cc98d3eab9a
gt-min <gt_min@naver.com>
2024-05-23 16:14:56 +0900

    20240523

--- 07_exception/src/pkg03_unchecked/UncheckedEx.java
+++ 07_exception/src/pkg03_unchecked/UncheckedEx.java
@@ -6,7 +6,7 @@ public class UncheckedEx {
     
     /*
      * unchecked exception
-     * 1. try-catch 구문이 없어도 동작하는 예외를 의미한다.
+     * 1. 예외 처리 구문(try-catch, throws)이 없어도 동작하는 예외를 의미한다.
      * 2. 실행 후에 예외 발생 여부를 확인할 수 있다.
      * 3. RuntimeException 클래스와 그 하위 클래스를 의미한다.
      */
--- 07_exception/src/pkg04_checked/CheckedEx.java
+++ 07_exception/src/pkg04_checked/CheckedEx.java
@@ -5,16 +5,17 @@ public class CheckedEx {
   public static void main(String[] args) {
     
     /*
-     * 클래스 호출 방식
-     * 1. 패키지와 클래스를 함께 작성해야 한다.
-     * 2. 패키지를 생략할 수 있는 경우가 있다.
-     *   1) 같은 패키지에 저장된 클래스
-     *   2) java.lang 패키지에 저장된 클래스 (System, String 등)
-     * 3. 패키지를 생략할 수 없다면 패키지를 작성해야 한다.
-     *   1) import 패키지.클래스 : 한 번만 작성
-     *   2) 패키지.클래스        : 매 번 작성
+     * checked exception
+     * 1. 예외 처리 구문(try-catch, throws)이 없으면 동작하지 않는 예외를 의미한다.
+     * 2. 실행 전에 미리 예외 처리를 해야 한다.
+     * 3. RuntimeException 클래스이 아닌 예외 클래스들이다.
      */
     
+    try {
+      Class.forName("oracle.jdbc.OracleDriver");
+    } catch(ClassNotFoundException e) {
+      System.out.println("해당 클래스를 찾을 수 없습니다.");
+    }
 
   }
 
