92f4c61ce3e0e9495470e4089f54ac6919512f30
gt-min <gt_min@naver.com>
2024-05-24 14:54:34 +0900

    20240524

---
+++ 08_collection_framework/src/pkg01_wrapper_class/WrapperEx.java
@@ -0,0 +1,31 @@
+package pkg01_wrapper_class;
+
+public class WrapperEx {
+
+  public static void main(String[] args) {
+
+    /*
+     *    primitive type      reference type (wrapper class)
+     * 1. byte                Byte
+     * 2. short               Short
+     * 3. int                 Integer
+     * 4. long                Long
+     * 5. float               Float
+     * 6. double              Double
+     * 7. char                Character
+     * 8. boolean             Boolean
+     */
+    
+    // Boxing (primitive type → reference type)
+    Integer number = 10;
+    
+    // UnBoxing (reference type → primitive type)
+    int number2 = number;
+    
+    System.out.println(number);
+    System.out.println(number2);
+    System.out.println(number + number2);
+
+  }
+
+}
